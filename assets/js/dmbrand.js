152f02fe11d5eb4f814190f535b3c110eb8229b2
AnishElon <anishelon@gmail.com>
2024-04-27 17:47:00 +0530

    website2

--- assets/js/dmbrand.js
+++ assets/js/dmbrand.js
@@ -5,4 +5,42 @@ $(".client-slider").slick({
   autoplaySpeed: 2000,
   dots: false,
   infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,. 
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        infinite: true,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
