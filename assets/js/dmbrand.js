b179bfb62ef71ce8fbd1208ece430a1f1f1a36b1
AnishElon <anishelon@gmail.com>
2024-04-28 00:05:56 +0530

    website2

--- assets/js/dmbrand.js
+++ assets/js/dmbrand.js
@@ -9,7 +9,7 @@ $(".client-slider").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
